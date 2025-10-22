ad474550eee9eb5a91bad6ef567e4cdee1edbf84
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2025-10-22 09:09:19 -0300

    Add files via upload

--- Tabela-Nutri-o-main/js/remove-paciente.js
+++ Tabela-Nutri-o-main/js/remove-paciente.js
@@ -1,5 +1,5 @@
 pacientes.forEach(function(paciente){
     paciente.addEventListener('dblclick', function(){
-        alert("Este eleento foi clicado duas vezes");
+        this.remove();
     });
 });
